26c43076ea4044e184dd3dca4f76fb18922a3bf5
honeycomb80 <honeycomb8080@gmail.com>
2014-05-05 16:03:52 -0700

    created model, added seeds, created controller, index route, index page, migrated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+tests = Test.create([
+	{prompt: "st.          = 2", answer: "length"},
+	{prompt: "stri.          = 4", answer: "length"},
+	{prompt: "string.          = gnirts", answer: "reverse"},
+	{prompt: "gnirts.          = string", answer: "reverse"},
+	{prompt: "string.          = STRING", answer: "upcase"},
+	{prompt: "Gnirts.          = GNIRTS", answer: "upcase"},
+	{prompt: "STRING.          = string", answer: "downcase"},
+	{prompt: "Gnirts.          = gnirts", answer: "downcase"}
+	])
